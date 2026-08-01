1149e201976bb1a0b0e7c60abaaffc0abcf1b2a8
Faiza-Alatf <hussainraoaltaf@gmail.com>
2026-08-01 12:18:31 +0500

    Reuse shared API service for authentication

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,21 +1,25 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api/auth";
+import api from "./api";
 
 export const registerUser = async (userData) => {
-    const response = await axios.post(
-        `${API_URL}/register`,
-        userData
-    );
+const response = await api.post(
+"/auth/register",
+userData
+);
 
-    return response.data;
+
+return response.data;
+
+
 };
 
 export const loginUser = async (loginData) => {
-    const response = await axios.post(
-        `${API_URL}/login`,
-        loginData
-    );
+const response = await api.post(
+"/auth/login",
+loginData
+);
 
-    return response.data;
+
+return response.data;
+
+
 };
