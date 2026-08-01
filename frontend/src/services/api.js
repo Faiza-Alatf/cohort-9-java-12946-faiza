e2d8659bed37879119916220ba5fba78a219fe8d
Faiza-Alatf <hussainraoaltaf@gmail.com>
2026-08-01 13:08:41 +0500

    Add API request timeout

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,10 +2,16 @@ import axios from "axios";
 
 const api = axios.create({
 baseURL: "http://localhost:8080/api",
+
 headers: {
 "Content-Type": "application/json",
 },
+
+// Allow browser to send HttpOnly JWT cookie
 withCredentials: true,
+
+// Prevent requests from hanging indefinitely
+timeout: 10000,
 });
 
 export default api;
