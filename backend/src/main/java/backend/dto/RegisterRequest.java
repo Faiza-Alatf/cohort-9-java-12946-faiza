b9c26b5eba7d039c7fe1b696677af67d84573781
Faiza-Alatf <hussainraoaltaf@gmail.com>
2026-08-01 19:46:23 +0500

    Add registration input validation

--- backend/src/main/java/backend/dto/RegisterRequest.java
+++ backend/src/main/java/backend/dto/RegisterRequest.java
@@ -1,71 +1,81 @@
 package backend.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class RegisterRequest {
 
-    @NotBlank(message = "First name is required")
-    private String firstName;
 
-    @NotBlank(message = "Last name is required")
-    private String lastName;
+@NotBlank(message = "First name is required")
+private String firstName;
 
-    private String email;
+@NotBlank(message = "Last name is required")
+private String lastName;
 
-    private String phone;
+@Email(message = "Invalid email format")
+private String email;
 
-    @NotBlank(message = "Password is required")
-    private String password;
+private String phone;
 
-    public RegisterRequest() {
-    }
+@NotBlank(message = "Password is required")
+@Size(
+        min = 8,
+        message = "Password must be at least 8 characters"
+)
+private String password;
 
-    public RegisterRequest(String firstName, String lastName, String email,
-                           String phone, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-    }
+public RegisterRequest() {
+}
 
-    public String getFirstName() {
-        return firstName;
-    }
+public RegisterRequest(String firstName, String lastName, String email,
+                       String phone, String password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.password = password;
+}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+public String getFirstName() {
+    return firstName;
+}
 
-    public String getLastName() {
-        return lastName;
-    }
+public void setFirstName(String firstName) {
+    this.firstName = firstName;
+}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+public String getLastName() {
+    return lastName;
+}
 
-    public String getEmail() {
-        return email;
-    }
+public void setLastName(String lastName) {
+    this.lastName = lastName;
+}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+public String getEmail() {
+    return email;
+}
 
-    public String getPhone() {
-        return phone;
-    }
+public void setEmail(String email) {
+    this.email = email;
+}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+public String getPhone() {
+    return phone;
+}
 
-    public String getPassword() {
-        return password;
-    }
+public void setPhone(String phone) {
+    this.phone = phone;
+}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+public String getPassword() {
+    return password;
+}
+
+public void setPassword(String password) {
+    this.password = password;
+}
+
+
 }
