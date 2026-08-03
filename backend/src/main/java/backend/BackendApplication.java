0d8dca2d12794a3c451b8547a4f23cc4a04ce9ff
Faiza-Alatf <hussainraoaltaf@gmail.com>
2026-08-03 13:28:06 +0500

    Restore Spring Boot application entry point

--- backend/src/main/java/backend/BackendApplication.java
+++ backend/src/main/java/backend/BackendApplication.java
@@ -1,14 +1,14 @@
 package backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-@ActiveProfiles("test")
-class BackendApplicationTests {
+@SpringBootApplication
+public class BackendApplication {
 
-    @Test
-    void contextLoads() {
-    }
+public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+}
+
+
 }
